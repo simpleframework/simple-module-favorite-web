4d398015f4fdd6f2c6109d1e4f71edfab51a5e73
simpleframework <cknet@126.com>
2013-11-15 11:40:11 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/favorite/web/page/t1/FavoritesMgrPage.java
+++ src/main/java/net/simpleframework/module/favorite/web/page/t1/FavoritesMgrPage.java
@@ -43,7 +43,7 @@ import net.simpleframework.mvc.template.t1.T1ResizedTemplatePage;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 @PageMapping(url = "/favorites/mgr")

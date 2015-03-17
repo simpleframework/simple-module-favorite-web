f812ae3c0b02a326b8bd068d91333ad9c32b6360
CHENKAN <CHENKAN@CHENKAN-PC>
2015-03-17 13:25:22 +0800

    Signed-off-by: CHENKAN <CHENKAN@CHENKAN-PC>

--- src/main/java/net/simpleframework/module/favorite/web/FavoriteWebContext.java
+++ src/main/java/net/simpleframework/module/favorite/web/FavoriteWebContext.java
@@ -1,7 +1,6 @@
 package net.simpleframework.module.favorite.web;
 
 import static net.simpleframework.common.I18n.$m;
-import net.simpleframework.ctx.Module;
 import net.simpleframework.ctx.ModuleFunctions;
 import net.simpleframework.module.favorite.impl.FavoriteContext;
 import net.simpleframework.module.favorite.web.page.MyFavoritesTPage;
@@ -25,25 +24,20 @@ public class FavoriteWebContext extends FavoriteContext implements IFavoriteWebC
 	}
 
 	@Override
-	protected Module createModule() {
-		return super.createModule().setDefaultFunction(FUNC_FAVORITES_MGR);
-	}
-
-	@Override
 	protected ModuleFunctions getFunctions() {
-		return ModuleFunctions.of(FUNC_FAVORITES_MGR, FUNC_MY_FAVORITES);
+		return ModuleFunctions.of(
+				new WebModuleFunction(this, FavoritesMgrPage.class).setName(
+						MODULE_NAME + "-FavoritesMgrPage").setText($m("FavoriteWebContext.1")),
+				new WebModuleFunction(this)
+						.setUrl(getUrlsFactory().getFavoriteUrl(null, MyFavoritesTPage.class, 0))
+						.setName(MODULE_NAME + "-MyFavoritesTPage").setText($m("FavoriteWebContext.0"))
+						.setDisabled(true));
 	}
 
 	@Override
 	public AbstractElement<?> toMyFavoriteElement(final PageParameter pp) {
-		return new LinkElement(FUNC_MY_FAVORITES.getText()).setHref(FUNC_MY_FAVORITES.getUrl());
+		final WebModuleFunction f = (WebModuleFunction) getFunctionByName(MODULE_NAME
+				+ "-MyFavoritesTPage");
+		return new LinkElement(f.getText()).setHref(f.getUrl());
 	}
-
-	public WebModuleFunction FUNC_FAVORITES_MGR = (WebModuleFunction) new WebModuleFunction(this,
-			FavoritesMgrPage.class).setName(MODULE_NAME + "-FavoritesMgrPage").setText(
-			$m("FavoriteWebContext.1"));
-	public WebModuleFunction FUNC_MY_FAVORITES = (WebModuleFunction) new WebModuleFunction(this)
-			.setUrl(getUrlsFactory().getFavoriteUrl(null, MyFavoritesTPage.class, 0))
-			.setName(MODULE_NAME + "-MyFavoritesTPage").setText($m("FavoriteWebContext.0"))
-			.setDisabled(true);
 }
